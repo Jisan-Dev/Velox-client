80c2914b1cf9febbc56477ae8fa8290b5a6bcdd5
Jisun696 <istiakkashem35@gmail.com>
2024-06-02 03:01:51 +0600

    initial setup done and started header

---
+++ src/pages/homepage/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+const Home = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>VELOX | HOME</title>
+      </Helmet>
+      <h1 className="text-3xl ">Hello world!!!!!!!!!!</h1>
+    </div>
+  );
+};
+
+export default Home;
