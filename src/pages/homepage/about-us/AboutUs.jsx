29d31aca2e8cc5e8b284072487946d0e5c6d5b41
Jisun696 <istiakkashem35@gmail.com>
2024-06-03 23:58:22 +0600

    started adding the aboutus section

---
+++ src/pages/homepage/about-us/AboutUs.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <section className="flex gap-2 flex-wrap">
+      <div>
+        <h2 className="text-xl">About Us</h2>
+        <p className="text-base">
+          We&apos;re passionate about empowering people to reach their fitness goals. We believe that everyone deserves to feel confident and strong, and our fitness tracker is
+          designed to be your supportive companion on your wellness journey. We&apos;re a team of fitness enthusiasts, data geeks, and design mavens who are dedicated to creating
+          innovative tools that make tracking progress and staying motivated easier than ever.
+        </p>
+      </div>
+      <div></div>
+    </section>
+  );
+};
+
+export default AboutUs;
