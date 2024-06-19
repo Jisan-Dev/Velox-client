d4867abb64b314e479c665c69f59716cee9e6e0c
Jisun696 <istiakkashem35@gmail.com>
2024-06-19 22:08:25 +0600

    added useAxiosSecyre hook

---
+++ src/hooks/useAxiosSecure.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import useAuth from './useAuth';
+import { useNavigate } from 'react-router-dom';
+
+const axiosSecure = axios.create({
+  baseURL: 'http://localhost:5000',
+});
+
+const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
+
+  // Add a request interceptor
+  // request interceptor to add authorization header for every secure call to the api
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      // Do something before request is sent
+      const token = localStorage.getItem('access-token');
+      // console.log('request stopped by interceptor before sent', token);
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
+
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (error) => {
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      // Do something with response error
+      // we'll handle 401 & 403 status
+      const status = error.response?.status;
+      // console.log('errorStatus check in the response interceptor', status);
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate('/login', { replace: true });
+      }
+
+      return Promise.reject(error);
+    }
+  );
+
+  return axiosSecure;
+};
+
+export default useAxiosSecure;
