85ac58a673a18293026acffd97baf24197d4d784
Jisun696 <istiakkashem35@gmail.com>
2025-01-24 15:32:23 +0600

    change image format of hero

--- src/pages/homepage/hero/Hero.jsx
+++ src/pages/homepage/hero/Hero.jsx
@@ -1,22 +1,22 @@
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-fade';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/pagination";
 
-import './hero.css';
+import "./hero.css";
 // import required modules
-import { EffectFade, Pagination, Autoplay } from 'swiper/modules';
-import { Button } from '@/components/ui/button';
-import { Link } from 'react-router-dom';
-import hero1 from '../../../assets/images/hero1.jpg';
-import hero2 from '../../../assets/images/hero2.jpg';
+import { EffectFade, Pagination, Autoplay } from "swiper/modules";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+import hero1 from "../../../assets/images/hero1.webp";
+import hero2 from "../../../assets/images/hero2.webp";
 
 const Hero = () => {
   return (
     <Swiper
-      effect={'fade'}
+      effect={"fade"}
       pagination={{ clickable: true }}
       // loop={true}
       modules={[Pagination, Autoplay, EffectFade]}
